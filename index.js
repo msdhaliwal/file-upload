ed811748c2b261868fb81d599639c6c7e3912c0e
msdhaliwal <msdhaliwal@outlook.in>
2021-07-27 01:41:47 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let multer = require("multer");
 let app = express();
+let bodyParser = require('body-parser')
+let jsonParser = bodyParser.json()
 let storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, "./uploads");
@@ -9,20 +11,33 @@ let storage = multer.diskStorage({
     callback(null, file.originalname);
   },
 });
-const upload = multer({ storage: storage }).single("myfile");
+
+let cpUpload = multer({ dest: 'uploads/' }).fields([{ name: 'file', maxCount: 1 }, { name: 'myfile2', maxCount: 1 }])
+const upload = multer({ storage: storage }).single("file");
 
 app.use(express.static("public"));
 
 app.post("/upload", function (req, res) {
+  console.log(0);
   upload(req, res, function (err) {
     if (err) {
       return res.end("Error uploading file.");
     }
-    // res.set({'Content-Type': 'application/json; charset=utf-8'})
+    console.log(req.file);
     return res.send({'age': 25});
   });
 });
 
+app.post('/upload2', cpUpload, function (req, res, next) {
+  cpUpload(req, res, function (err) {
+    if (err) {
+      // A Multer error occurred when uploading.
+      return res.json({code: 'error1'})
+    }
+    return res.json({code: 'all good', files: req.files})
+    // Everything went fine.
+  });
+});
 app.listen(2000, function () {
   console.log("Server is running on port 2000");
 });
